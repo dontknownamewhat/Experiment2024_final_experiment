99fcdd4299b7afc0dff12ef7d8dfce1dc8511abd
czq <zhaoqicai97@gmail.com>
2024-09-19 20:44:34 +0800

    set a new buttonview in activity_hello1841.xml

--- app/src/main/java/com/example/casper/Experiment2024/Hello1841Activity.java
+++ app/src/main/java/com/example/casper/Experiment2024/Hello1841Activity.java
@@ -1,7 +1,8 @@
 package com.example.casper.Experiment2024;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Hello1841Activity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,7 @@ public class Hello1841Activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView textView = findViewById(R.id.textview_helloworld);
+        textView.setText(R.string.hello_jnu);  // 动态设置为 hello_jnu
     }
 }
