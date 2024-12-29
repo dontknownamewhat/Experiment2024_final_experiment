c7f604c3fe92007521ee25eb8a07e05f69bfb8c9
czq <zhaoqicai97@gmail.com>
2024-12-29 21:16:54 +0800

    实现一个打卡记录一张照片

--- app/src/main/java/com/example/casper/Experiment2024/Hello1841Activity.java
+++ app/src/main/java/com/example/casper/Experiment2024/Hello1841Activity.java
@@ -5,7 +5,16 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,46 +25,35 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
-import com.example.casper.Experiment2024.model.CheckIn;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
+import com.example.casper.Experiment2024.model.CheckIn;
 import com.example.casper.Experiment2024.model.CheckInViewModel;
 import com.example.casper.Experiment2024.adapter.CheckInAdapter;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import android.content.Intent;
 import android.location.Location;
-import android.net.Uri;
 import android.provider.MediaStore;
-import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Hello1841Activity extends AppCompatActivity {
 
     private CheckInViewModel checkInViewModel;
     private RecyclerView recyclerView;
     private CheckInAdapter adapter;
     FusedLocationProviderClient fusedLocationClient;
-    private static final int TAKE_PHOTO_REQUEST_TWO = 444;
-    private static final int TAKE_PHOTO_REQUEST_ONE = 333;
-    private static final int TAKE_PHOTO_REQUEST_THREE = 555;
-    // 集成拍照功能的相关变量
+
+    // UI组件
     private Button btn_take_photo, btn_choose_photo, btnAddCheckIn;
     private ImageView iv_image;
     private static final int TAKE_PHOTO_REQUEST = 333;
     private static final int CHOOSE_PHOTO_REQUEST = 444;
     private Uri selectedImageUri;  // 存储选择或拍摄的图片 URI
-    private File photoFile;
-
     private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,12 +64,9 @@ public class Hello1841Activity extends AppCompatActivity {
         // 设置 TabLayout 和 ViewPager2
         ViewPager2 viewPager = findViewById(R.id.view_pager);
         TabLayout tabs = findViewById(R.id.tabs);
-
-        // 创建 Adapter 并设置给 ViewPager2
         TabPagerAdapter adapterViewPager = new TabPagerAdapter(this);
         viewPager.setAdapter(adapterViewPager);
 
-        // 将 TabLayout 和 ViewPager2 关联起来
         new TabLayoutMediator(tabs, viewPager, (tab, position) -> {
             switch (position) {
                 case 0:
@@ -86,20 +81,12 @@ public class Hello1841Activity extends AppCompatActivity {
             }
         }).attach();
 
-        // 初始化打卡相关的UI组件
+        // 初始化UI组件
         EditText etDescription = findViewById(R.id.etDescription);
         btnAddCheckIn = findViewById(R.id.btnAddCheckIn);
         recyclerView = findViewById(R.id.rvCheckIns);
+        iv_image = findViewById(R.id.iv_image);
 
-        // 动态设置最大高度
-        recyclerView.post(() -> {
-            int maxHeight = (int) (20 * getResources().getDisplayMetrics().density); // 最大高度200dp
-            if (recyclerView.getHeight() > maxHeight) {
-                ViewGroup.LayoutParams params = recyclerView.getLayoutParams();
-                params.height = maxHeight;
-                recyclerView.setLayoutParams(params);
-            }
-        });
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -126,13 +113,12 @@ public class Hello1841Activity extends AppCompatActivity {
                     if (location != null) {
                         double latitude = location.getLatitude();
                         double longitude = location.getLongitude();
+
                         // 获取图片的路径
-                        String imagePath = photoFile.getAbsolutePath();
+                        String imagePath = selectedImageUri != null ? selectedImageUri.getPath() : null;
+
                         // 创建一个新的 CheckIn 对象并插入
-                        CheckIn checkIn = new CheckIn(description, latitude, longitude,imagePath);
-                        if (selectedImageUri != null) {
-                            checkIn.setImagePath(selectedImageUri.toString());  // 保存照片路径
-                        }
+                        CheckIn checkIn = new CheckIn(description, latitude, longitude, imagePath);
                         checkInViewModel.insert(checkIn);
                         Log.d("LocationCheck", "Latitude: " + latitude + ", Longitude: " + longitude);
                         etDescription.setText(""); // 清空输入框
@@ -144,134 +130,63 @@ public class Hello1841Activity extends AppCompatActivity {
             }
         });
 
-        // 集成拍照功能的相关代码
+        // 拍照按钮
         btn_take_photo = findViewById(R.id.button_take_photo);
+        btn_take_photo.setOnClickListener(view -> {
+            Uri imageUri = createImageUri(Hello1841Activity.this);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST);
+        });
+
+        // 从相册选择图片按钮
         btn_choose_photo = findViewById(R.id.button_select_photo);
-        iv_image = findViewById(R.id.iv_image);
-
-        btn_take_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageUri = createImageUri(Hello1841Activity.this);
-                Intent intent = new Intent();
-                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); // 如果不设置EXTRA_OUTPUT getData() 获取的是bitmap数据  是压缩后的
-                startActivityForResult(intent, TAKE_PHOTO_REQUEST_ONE);
-                try {
-                    imageUri = TakePhotoUtils.takePhoto(Hello1841Activity.this, TAKE_PHOTO_REQUEST_THREE);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-        });
-
-        btn_choose_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                pickImageFromAlbum();
-            }
-        });
+        btn_choose_photo.setOnClickListener(view -> pickImageFromAlbum());
     }
+
+    // 从相册选择图片
     public void pickImageFromAlbum() {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
-        startActivityForResult(intent, 111);
-
+        startActivityForResult(intent, CHOOSE_PHOTO_REQUEST);
     }
+
     // 处理拍照和选择照片结果
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            case 111:
-            case 222:
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(Hello1841Activity.this, "点击取消从相册选择", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                try {
-                    Uri imageUri = data.getData();
-                    Log.e("TAG", imageUri.toString());
-                    iv_image.setImageURI(imageUri);
-                } catch (Exception e) {
-                    e.printStackTrace();
+            case CHOOSE_PHOTO_REQUEST:
+                if (resultCode == RESULT_OK && data != null) {
+                    selectedImageUri = data.getData();
+                    iv_image.setImageURI(selectedImageUri);
                 }
                 break;
-            case TAKE_PHOTO_REQUEST_ONE:
-                if (resultCode == RESULT_CANCELED) {
-                    delteImageUri(Hello1841Activity.this, imageUri);
-                    Toast.makeText(Hello1841Activity.this, "点击取消  拍照", Toast.LENGTH_LONG).show();
-                    return;
-                }
 
-                try {
-                    //如果拍照图片过大会无法显示
-                    Bitmap bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                    iv_image.setImageBitmap(bitmap1);
-                } catch (Exception e) {
-                    e.printStackTrace();
+            case TAKE_PHOTO_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    iv_image.setImageURI(imageUri);
+                    selectedImageUri = imageUri;  // 保存拍照的图片 URI
                 }
                 break;
-            case TAKE_PHOTO_REQUEST_TWO:
-                if (resultCode == RESULT_CANCELED) {
-                    delteImageUri(Hello1841Activity.this, imageUri);
-                    return;
-                }
-                Bitmap photo = data.getParcelableExtra("data");
-                iv_image.setImageBitmap(photo);
-                break;
-            case TAKE_PHOTO_REQUEST_THREE:
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(Hello1841Activity.this, "点击取消从相册选择", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                Bitmap bitmap = BitmapFactory.decodeFile(imageUri.getPath(), getOptions(imageUri.getPath()));
-                iv_image.setImageBitmap(bitmap);
-                break;
+
             default:
                 break;
         }
     }
 
-
+    // 创建图片 URI
     private static Uri createImageUri(Context context) {
         String name = "takePhoto" + System.currentTimeMillis();
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.Images.Media.TITLE, name);
         contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, name + ".jpeg");
         contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        Uri uri = context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-        return uri;
+        return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
     }
 
-
-    public static void delteImageUri(Context context, Uri uri) {
-        context.getContentResolver().delete(uri, null, null);
-
-    }
-
-
-    /**
-     * 获取压缩图片的options
-     *
-     * @return
-     */
-    public static BitmapFactory.Options getOptions(String path) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-        options.inPreferredConfig = Bitmap.Config.RGB_565;
-        options.inSampleSize = 4;      //此项参数可以根据需求进行计算
-        options.inJustDecodeBounds = false;
-
-        return options;
-    }
     // 定义 TabPagerAdapter 类
     private static class TabPagerAdapter extends FragmentStateAdapter {
-
         public TabPagerAdapter(@NonNull AppCompatActivity activity) {
             super(activity);
         }
@@ -296,5 +211,4 @@ public class Hello1841Activity extends AppCompatActivity {
             return 3; // 总共有三个 Tab
         }
     }
-
 }
